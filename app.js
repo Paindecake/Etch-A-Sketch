60d73a89e16fc3d194a547ed40b06c1ddc78889d
pancake <leomassfelder@outlook.fr>
2021-07-06 17:07:36 +0200

    Added mouse eventlistener to sketch

--- app.js
+++ app.js
@@ -6,10 +6,39 @@ function createGrid(gridNumber) {
 	for (i = 0; i < gridArea; i++) {
 		let divGrid = document.createElement("div");
 		divGrid.className = `grid${i}`;
-		divGrid.textContent = `${i}`;
 		grid.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
 		grid.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
 		grid.insertAdjacentElement("beforeend", divGrid);
 	}
 }
 createGrid(10);
+
+const mhover = document.querySelectorAll("#grid > div");
+// addEventListener("click", (e) => {
+// 	console.log(e.target);
+// });
+
+function sketchOnClick() {
+	grid.addEventListener("click", (e) => {
+		e.target.classList.toggle("sketch");
+		console.log(e.target);
+	});
+}
+// sketchOnClick();
+
+function sketchOnHover() {
+	grid.addEventListener("mouseover", (e) => {
+		e.target.classList.add("sketch");
+		console.log(e.target);
+	});
+}
+
+sketchOnHover();
+
+function sketchOnHoverOut() {
+	grid.addEventListener("mouseover", (e) => {
+		e.target.classList.remove("sketch");
+	});
+}
+
+// sketchOnHoverOut();
